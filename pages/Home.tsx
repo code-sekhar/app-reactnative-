7c8ee88835f4654a8a19ed84f7087c367ffe57e1
sekhar chaudhaudhary <sekharchaudhary5@gmail.com>
2025-01-01 16:54:43 +0530

    feat:add App

---
+++ pages/Home.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function Home(props: { name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) {
+    return (
+        <View>
+            <Text style={styles.text}>Home button</Text>
+            <Text style={styles.text}>Welcome {props.name} off Our Website</Text>
+        </View>
+    );
+}
+export default Home;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+        width: 50,
+        height: 50,
+    },
+    text: {
+        fontSize: 20,
+        marginBottom: 20,
+    },
+    button: {
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+});
